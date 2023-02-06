d0d400e6af892b299a42e28bee33ea2f00424a61
Saba pachulia <sabapachulia123@gmail.com>
2023-02-06 17:01:13 +0400

    turn names to  lowerCamelCase

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {BrowserRouter,Route,Routes} from "react-router-dom";
-import Navbar from './components/navbar';
-import Allurls from './pages/All-Urls';
-import Decode from './pages/Decode-Urls';
-import Encode from './pages/Encode-Urls';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Allurls from './pages/AllUrls';
+import Decode from './pages/DecodeUrls';
+import Encode from './pages/EncodeUrls';
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Encode />} />
-      <Route path='/decode' element={<Decode />} />
-      <Route path='/allUrls' element={<Allurls />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Encode />} />
+        <Route path="/decode" element={<Decode />} />
+        <Route path="/allUrls" element={<Allurls />} />
+      </Routes>
     </BrowserRouter>
   );
 }
